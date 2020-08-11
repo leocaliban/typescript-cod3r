80bfe772acdf8bfca9fb1b4661639eae7628162d
Leocaliban <leocaliban@gmail.com>
2020-08-10 23:21:20 -0300

    Cria tasks e gera build

--- 07_ts_js_libs/02_gulp/gulpfile.js
+++ 07_ts_js_libs/02_gulp/gulpfile.js
@@ -4,16 +4,24 @@ const browserify = require('browserify');
 const source = require('vinyl-source-stream');
 const tsify = require('tsify');
 
-function clearDist(cb) {
-    cb();
+function clearDist() {
+    return del(['dist']);
 }
 
-function copyHTML(cb) {
-    cb();
+function copyHTML() {
+    return src('public/**/*')
+        .pipe(dest('dist'));
 }
 
-function generateJS(cb) {
-    cb();
+function generateJS() {
+    return browserify({
+        basedir: '.',
+        entries: ['src/main.ts']
+    })
+        .plugin(tsify)
+        .bundle()
+        .pipe(source('app.js'))
+        .pipe(dest('dist'));
 }
 
 exports.default = series(
--- 07_ts_js_libs/02_gulp/src/main.ts
+++ 07_ts_js_libs/02_gulp/src/main.ts
@@ -2,3 +2,6 @@ import Livro from './model/livro';
 
 const livro = new Livro('Candyman', 33.30, 0.10);
 console.log(livro.precoComDesconto());
+
+document.write(`<h2 class='item'>${livro.nome}</h2>`);
+document.write(`<h3 class='item'>${livro.precoComDesconto()}</h3>`);
